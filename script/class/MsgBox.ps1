b3b1734e8d79ac0d7f09ffb3da4014ee4335c045
takumi-t4096 <takumi_t@outlook.com>
2023-05-27 12:05:35 +0900

    add: MsgBox.ps1ファイルを追加
    ワーニングメッセージを表示する機能が入っています

--- script/class/MsgBox.ps1
+++ script/class/MsgBox.ps1
@@ -1,13 +1,33 @@
-﻿# MsgBox class
+﻿# MsgBox Class
+
+# アセンブリのロード
+Add-Type -AssemblyName System.Windows.Forms
 
 class MsgBox {
+    [string] $msgStr = ""
+    [string] $msgTitle = ""
+    [string] $msgBtn = ""
+    [string] $msgIcon = ""
+    [string] $msgBtnPosition = ""
     
-    [
-
-    MsgBox() {
-        Add-Type -Assembly System.Windows.Forms
-    }
+    # コンストラクタ
+    MsgBox() {}
 
     # ワーニング
+    warningMsg([string] $msgStr) {
+        $this.msgStr = $msgStr
+        $this.msgTitle = "警告メッセージ"
+        $this.msgBtn = "OK"
+        $this.msgIcon = "Exclamation"
+        $this.msgBtnPosition = "button3"
+
+        [System.Windows.Forms.MessageBox]::Show(
+            $this.msgStr,
+            $this.msgTitle, 
+            $this.msgBtn,
+            $this.msgIcon,
+            $this.msgBtnPosition
+        )
+    }
 
 }
